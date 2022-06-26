ca420bd75fda7ceadc86347e9ab982f16efa577c
iamalmiir <iamalmiir@gmail.com>
2022-06-26 19:35:09 -0400

    set security headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,46 @@
 /** @type {import('next').NextConfig} */
+
+const headers = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'same-origin',
+  },
+]
+
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers,
+      },
+    ]
+  },
+
   reactStrictMode: true,
   swcMinify: true,
   images: {
