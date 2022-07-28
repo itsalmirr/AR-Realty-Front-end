5311ba1ca198f6921f7f761c7f7a1a7d6274be66
iamalmiir <iamalmiir@gmail.com>
2022-07-28 08:49:46 -0400

    improved dashboard stability

--- src/components/CustomAOS.jsx
+++ src/components/CustomAOS.jsx
@@ -18,15 +18,14 @@ const CustomAOS = () => {
   return (
     <ToastContainer
       position='top-right'
-      autoClose={5000}
+      autoClose={3000}
       hideProgressBar={false}
       newestOnTop
       closeOnClick
       rtl={false}
-      pauseOnFocusLoss
       draggable
-      pauseOnHover
-      limit={3}
+      pauseOnHover={false}
+      limit={1}
       transition={Flip}
       theme='colored'
     />
