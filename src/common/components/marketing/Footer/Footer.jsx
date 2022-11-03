2dc96a67d631be3e5d7403c8a541fb7af4a3c770
iamalmiir <iamalmiir@gmail.com>
2022-11-03 19:21:23 -0400

    small updates

--- src/common/components/marketing/Footer/Footer.jsx
+++ src/common/components/marketing/Footer/Footer.jsx
@@ -12,15 +12,16 @@ const Footer = () => {
         <div className='pt-5 pb-5 md:flex md:items-center md:justify-between'>
           <div className='flex space-x-6 md:order-2'>
             {socialMedia.map((item) => (
-  
-                <Link href={item.href} key={item.name}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='text-gray-200 my-4 hover:text-accentDark'>
-                  <span className='sr-only'>{item.name}</span>
-                  {item.icon}
-                </Link>
-            
+              <Link
+                href={item.href}
+                key={item.name}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='text-gray-200 my-4 hover:text-accentDark'
+              >
+                <span className='sr-only'>{item.name}</span>
+                {item.icon}
+              </Link>
             ))}
           </div>
           <p className='text-base text-gray-200 md:mt-0 md:order-1'>
