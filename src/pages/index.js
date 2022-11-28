c7968c07d8d4be7852c348dade1c701a76a89998
iamalmiirr <iamalmiir@gmail.com>
2022-11-28 01:21:14 -0500

    small improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,11 @@ import { Layout } from '@components/layouts'
 import { Home } from '@modules/home/index'
 
 const HomePage = ({ listings }) => {
-  console.log(listings)
-  return <Layout title={'Home'}>{/* <Home listings={listings} /> */}</Layout>
+  return (
+    <Layout title={'Home'}>
+      <Home listings={listings} />
+    </Layout>
+  )
 }
 
 export default HomePage
@@ -15,9 +18,6 @@ export const getStaticProps = async () => {
   const listings = await axios.get(
     `${API_URL}/api/listings/?page_size=3&page=1`
   )
-
-  const data = listings.data
-  console.log(data)
 
   return {
     props: {
