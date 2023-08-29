dfb3c1b514a05e66b4ba4971e5193dd7a89def02
Almir R. <r9@almir.io>
2023-08-29 18:15:04 -0400

    code update

--- src/pages/listings/searchpage.jsx
+++ src/pages/listings/searchpage.jsx
@@ -11,43 +11,46 @@ import { fetchListings } from '@common/queries/listings'
 import { Divider } from '@components/app/Divider'
 import { Spinner } from '@components/app/Spinner'
 import { NoResults } from '@components/app/NoResults'
+import { ServerError } from '@components/app/Error'
 
 const SearchPage = ({ listings }) => {
-  const router = useRouter()
-  const [results, setResults] = useState([])
-  const [loading, setLoading] = useState(results.length === 0)
+  // const router = useRouter()
+  // const [results, setResults] = useState([])
+  // const [loading, setLoading] = useState(results.length === 0)
 
-  const handleListingChange = async (query) => {
-    try {
-      const { results: data } = await quickSearch(query)
-      setResults(data)
-    } catch (err) {
-      toast.error(err.message)
-    }
-    setLoading(false)
-  }
+  // const handleListingChange = async (query) => {
+  //   try {
+  //     // const { results: data } = await quickSearch(query)
+  //     // setResults(data)
+  //     console.log('Server is down.')
+  //   } catch (err) {
+  //     toast.error(err.message)
+  //   }
+  //   setLoading(false)
+  // }
 
-  const renderResults = () => {
-    return (
-      <div>
-        {results.length === 0 ? (
-          <div>
-            <NoResults />
-          </div>
-        ) : (
-          <ListingsList listings={results} />
-        )}
-      </div>
-    )
-  }
+  // const renderResults = () => {
+  //   return (
+  //     <div>
+  //       {results.length === 0 ? (
+  //         <div>
+  //           <NoResults />
+  //         </div>
+  //       ) : (
+  //         <ListingsList listings={results} />
+  //       )}
+  //     </div>
+  //   )
+  // }
 
-  useEffect(() => {
-    handleListingChange(router.query.q)
-  }, [router.query.q])
+  // useEffect(() => {
+  //   handleListingChange(router.query.q)
+  // }, [router.query.q])
 
   return (
     <>
-      <header className='listing-page-cover'>
+      <ServerError />
+      {/* <header className='listing-page-cover'>
         <div className='bg-slate-600/80 p-12 text-center md:flex md:items-center md:justify-center'>
           <h2 className='flex justify-center items-center text-2xl font-bold leading-7 text-white sm:text-3xl sm:truncate'>
             <GiFamilyHouse className='w-8 h-8 mr-2' />
@@ -63,20 +66,20 @@ const SearchPage = ({ listings }) => {
         ) : (
           <Spinner />
         )}
-      </main>
+      </main> */}
     </>
   )
 }
 
-export const getStaticProps = async () => {
-  const listings = await fetchListings(`${API_URL}/api/listings/random/`)
+// export const getStaticProps = async () => {
+//   const listings = await fetchListings(`${API_URL}/api/listings/random/`)
 
-  return {
-    props: {
-      listings: listings.results,
-    },
-    revalidate: 60,
-  }
-}
+//   return {
+//     props: {
+//       listings: listings.results,
+//     },
+//     revalidate: 60,
+//   }
+// }
 
 export default SearchPage
